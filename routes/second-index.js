a6b6b09be0f3aa89f107a42f9ab09523349fb849
bwyfed <36172659@qq.com>
2018-01-02 17:51:50 +0800

    将所有的二级页面置于/second路由下，并支持“服务”和“行业及解决方案”的跳转。

---
+++ routes/second-index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by BWY on 2018/1/2.
+ */
+var express = require('express');
+var router = express.Router();
+
+router.get('/industry', function(req, res) {
+    console.log(111);
+    res.render('industry',{ title: '行业及解决方案'})
+});
+/* GET users listing. */
+router.get('/service/:subtype1',function(req, res){
+    console.log(222);
+    var subtype1 = req.params['subtype1'];
+    res.render('service', { title: '服务类型',type: subtype1 });
+});
+
+
+module.exports = router;

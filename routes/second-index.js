7e20c64e429dbac79521ed69007a871789c500b7
bwyfed <36172659@qq.com>
2018-01-02 18:31:56 +0800

    把相关的静态资源和模板都变成了设计的second名字。

--- routes/second-index.js
+++ routes/second-index.js
@@ -6,13 +6,13 @@ var router = express.Router();
 
 router.get('/industry', function(req, res) {
     console.log(111);
-    res.render('industry',{ title: '行业及解决方案'})
+    res.render('second',{ title: '行业及解决方案',type: 'industry'})
 });
 /* GET users listing. */
 router.get('/service/:subtype1',function(req, res){
     console.log(222);
     var subtype1 = req.params['subtype1'];
-    res.render('service', { title: '服务类型',type: subtype1 });
+    res.render('second', { title: '服务类型',type: subtype1 });
 });
 
 

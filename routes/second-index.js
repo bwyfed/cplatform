f2278fe0872540fae05c492e64e18a330a24eba2
bwyfed <36172659@qq.com>
2018-01-03 14:22:30 +0800

    实现“服务”之“产品技术”页面，并实现数据配置化。

--- routes/second-index.js
+++ routes/second-index.js
@@ -14,7 +14,7 @@ router.get('/industry', function(req, res) {
 router.get('/service/:subtype1',function(req, res){
     console.log(222);
     var subtype1 = req.params['subtype1'];
-    res.render('second', { title: '服务类型',type: 'service',subtype:subtype1, data: data_service["solution"] });
+    res.render('second', { title: '服务类型',type: 'service',subtype:subtype1, data: data_service[subtype1] });
 });
 
 

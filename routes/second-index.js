6784f64ed007de85d643175607c1ce45c1a550f7
bwyfed <36172659@qq.com>
2018-01-03 11:32:41 +0800

    实现“服务”之“解决方案”页面，并实现数据配置化。

--- routes/second-index.js
+++ routes/second-index.js
@@ -4,6 +4,8 @@
 var express = require('express');
 var router = express.Router();
 
+var data_service = require('../app/mock/service.json');
+console.log('wocao'); console.log(data_service);
 router.get('/industry', function(req, res) {
     console.log(111);
     res.render('second',{ title: '行业及解决方案',type: 'industry'})
@@ -12,7 +14,7 @@ router.get('/industry', function(req, res) {
 router.get('/service/:subtype1',function(req, res){
     console.log(222);
     var subtype1 = req.params['subtype1'];
-    res.render('second', { title: '服务类型',type: subtype1 });
+    res.render('second', { title: '服务类型',type: 'service',subtype:subtype1, data: data_service["solution"] });
 });
 
 

207edc7236756873d58e7f518289421d47fdaccb
bwyfed <36172659@qq.com>
2018-01-04 15:39:34 +0800

    实现页头部分各个菜单切换时，active的动态切换。

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,7 +2,32 @@
  * Created by BWY on 2018/1/2.
  */
 $(function(){
-    $("#navbar-collapse-1 li").click(function(){
-        $(this).addClass('active').siblings().removeClass('active');
+    var $navbarCollapseCtn = $("#navbar-collapse-1");
+    var activeItemClass = sessionStorage.getItem('activeItemClass') || '.nav-item-index';
+    var pathname = window.location.pathname,
+        pathmap = {
+            '/': '.nav-item-index',
+            '/second/service/solution': '.nav-item-service-ctn',
+            '/second/service/product': '.nav-item-service-ctn',
+            '/second/service/operate': '.nav-item-service-ctn',
+            '/second/industry': '.nav-item-industry',
+            '/second/customers': '.nav-item-customers',
+            '/second/about': '.nav-item-about'
+        };
+    if(activeItemClass!==pathmap[pathname]) {
+        activeItemClass = pathmap[pathname];
+    }
+    activeNavItem($navbarCollapseCtn,activeItemClass);
+    function activeNavItem($ctn,activeClass) {
+        $ctn.find(activeClass).addClass("active").siblings().removeClass("active");
+        sessionStorage.setItem('activeItemClass',activeClass);
+    }
+    $navbarCollapseCtn.find(".nav-item").click(function(){
+        var activeClass = $(this).data("active");
+        // activeNavItem($navbarCollapseCtn,activeClass);
+        sessionStorage.setItem('activeItemClass',activeClass);
     });
+    $("a.navbar-brand").click(function(){
+        sessionStorage.setItem('activeItemClass','.nav-item-index');
+    })
 });

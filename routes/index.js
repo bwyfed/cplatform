c486986cd01fa6ad07a80bce6f623e16bce0fe23
bwyfed <36172659@qq.com>
2018-01-15 14:20:27 +0800

    在首页中替换了banner图，并在首页中引入数据配置化。

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var data_index = require('../app/mock/index.json');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', data_index);
 });
 
 module.exports = router;
